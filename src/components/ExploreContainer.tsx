668e1718a5a54deed010000245bf542615f158cb
remultajustinekyle <justinekyleremulta2@gmail.com>
2025-02-12 01:02:25 +0800

    added intial

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -5,6 +5,7 @@ interface ContainerProps { }
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div id="container">
+      <h1>Hello World</h1>
       <strong>Ready to create an app?</strong>
       <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
     </div>
