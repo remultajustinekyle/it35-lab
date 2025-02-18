8400a7fc29d29f99f36dfec75d44d68e92b01f6a
remultajustinekyle <justinekyleremulta2@gmail.com>
2025-02-18 20:38:45 +0800

    updated features

---
+++ src/pages/Details.tsx
@@ -0,0 +1,30 @@
+
+import { 
+    IonBackButton,
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonMenuButton, 
+      IonPage, 
+      IonTitle, 
+      IonToolbar 
+  } from '@ionic/react';
+  
+  const Details: React.FC = () => {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar color="primary">
+            <IonButtons slot='start'>
+                <IonBackButton defaultHref='/it35a-lab/app/home'></IonBackButton>
+            </IonButtons>
+            <IonTitle>Home Details</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className='ion-padding'>
+        </IonContent>
+      </IonPage>
+    );
+  };
+  
+  export default Details;
