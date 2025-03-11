5212214922608d52ef10de6f197643dc868ec54a
remultajustinekyle <justinekyleremulta2@gmail.com>
2025-03-11 08:56:43 +0800

    added modal/login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,36 +1,62 @@
 
 import { 
-    IonButton,
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar, 
-      useIonRouter
-  } from '@ionic/react';
-  
-  const Login: React.FC = () => {
-    const navigation = useIonRouter();
+  IonButton,
+  IonButtons,
+    IonContent, 
+    IonHeader, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar, 
+    useIonRouter
+} from '@ionic/react';
+import { useState } from 'react';
 
-    const doLogin = () => {
-        navigation.push('/it35-lab/app','forward','replace');
-    }
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className='ion-padding'>
-            <IonButton onClick={() => doLogin()} expand="full">
+function Login() {
+  const navigation = useIonRouter();
+
+  const [showToast, setShowToast] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [email, setemail] = useState('');
+  const [password, setPasword] = useState('');
+
+
+  const user_email = 'justinekylremulta2@gmail.com';
+  const user_pwd = 'baranggan2002';
+
+  const doLogin = () => {
+
+    if (email !== user_email || password !== user_pwd) {
+      setShowAlert(true);
+      return;
+    } else {
+
+      console.log(email);
+      console.log(password);
+
+      setShowToast(true);
+      setTimeout(() => {
+
+        const doLogin = () => {
+          navigation.push('/it35-lab/app', 'forward', 'replace');
+        };
+        return (
+          <IonPage>
+            <IonHeader>
+              <IonToolbar>
+                <IonTitle>Login</IonTitle>
+              </IonToolbar>
+            </IonHeader>
+            <IonContent className='ion-padding'>
+              <IonButton onClick={() => doLogin()} expand="full">
                 Login
-            </IonButton>
-        </IonContent>
-      </IonPage>
-    );
+              </IonButton>
+            </IonContent>
+          </IonPage>
+        );
+      });
+
+      export default Login;
+    }
   };
-  
-  export default Login;
+}
