3d6c2c318c37c01dcb965f411590ea3a52ab975d
remultajustinekyle <justinekyleremulta2@gmail.com>
2025-03-17 05:51:37 +0800

    Updates

--- assets/ios.transition-legacy-yEo72F1e.js
+++ assets/ios.transition-legacy-yEo72F1e.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-I5opuhCP.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-Bfk5uxnA.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
