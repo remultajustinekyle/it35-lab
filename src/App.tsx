1b73baa6e9b72e4c9fb34ec3af9f08dcfc34aaf8
remultajustinekyle <justinekyleremulta2@gmail.com>
2025-04-08 08:39:35 +0800

    EditProfilePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Login from './pages/Login';
-import Menu from './pages/Menu';
-import Register from './pages/Register';
-
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -35,6 +31,9 @@ import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import Login from './pages/Login';
+import Menu from './pages/Menu';
+import Register from './pages/Register';
 
 setupIonicReact();
 
