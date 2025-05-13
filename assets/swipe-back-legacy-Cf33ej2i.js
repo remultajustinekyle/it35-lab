b62b9624280afd90a257802eed32970f8d25baea
remultajustinekyle <justinekyleremulta2@gmail.com>
2025-05-14 04:44:33 +0800

    Updates

--- assets/swipe-back-legacy-Cf33ej2i.js
+++ assets/swipe-back-legacy-Cf33ej2i.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-HYFPvlbj.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-DngezIFd.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
